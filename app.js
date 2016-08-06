9766b53d7f6e07bfec8d1bba2a08d59f3cf59002
Danny Lim <Danny@Dannys-MacBook-Pro.local>
2016-08-06 11:54:29 -0700

    Set up basic boiler plate

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+// CONSTANTS
+const PORT = process.env.PORT || 8000;
+
+// REQUIRES
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+
+// APP DECLARATION
+const app = express();
+
+// GENERAL MIDDLEWARE
+app.use(morgan('dev')); //console logs requests as we get them
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public')); //for front end
+
+// ROUTES
+
+
+// SERVER LISTEN
+app.listen(PORT, err => {
+  console.log(err || `Server listening on port ${PORT}`);
+});
